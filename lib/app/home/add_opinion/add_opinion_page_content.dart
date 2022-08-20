c14f21e96dbe8b7d2db27a8febf0878864418daf
Michał Kaczor <michal.l.kaczor@gmail.com>
2022-08-20 19:17:49 +0200

    after lesson 5

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,79 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
+    required this.onSave,
   }) : super(key: key);
+
+  final Function onSave;
+
+  @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var restaurantName = '';
+  var pieceName = '';
+  var rating = 3.0;
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration:
+                  const InputDecoration(hintText: 'Podaj nazwę cukierni'),
+              onChanged: (newValue) {
+                setState(() {
+                  restaurantName = newValue;
+                });
+              },
+            ),
+            TextField(
+              decoration:
+                  const InputDecoration(hintText: 'Podaj nazwę ciastka'),
+              onChanged: (newValue) {
+                setState(() {
+                  pieceName = newValue;
+                });
+              },
+            ),
+            Slider(
+              onChanged: (newValue) {
+                setState(() {
+                  rating = newValue;
+                });
+              },
+              value: rating,
+              min: 1.0,
+              max: 6.0,
+              divisions: 10,
+              label: rating.toString(),
+            ),
+            ElevatedButton(
+              onPressed: restaurantName.isEmpty || pieceName.isEmpty
+                  ? null
+                  : () {
+                      FirebaseFirestore.instance.collection('restaurants').add(
+                        {
+                          'name': restaurantName,
+                          'piece': pieceName,
+                          'rating': rating,
+                        },
+                      );
+                      widget.onSave();
+                    },
+              child: const Text('Dodaj'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
